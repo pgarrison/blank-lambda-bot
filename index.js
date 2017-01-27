d5db0af8e17c3fbd77d6dae422c18f2d6e276df3
Philip Garrison <philipgarrison@unu.edu>
2017-01-27 15:26:10 +0800

    Take lines sequentially, turn off wordfilter

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Twit = require('twit');
-var wordfilter = require('wordfilter');
-var T = new Twit(require('botfiles/config.js'));
+var config = require('botfiles/config.js');
+var T = new Twit(config);
 var myText = require('botfiles/sample-text.js');
 
 //a nice 'pick' function thanks to Darius Kazemi: https://github.com/dariusk
@@ -8,46 +8,59 @@ Array.prototype.pick = function() {
   return this[Math.floor(Math.random()*this.length)];
 };
 
-//functions 
-  function tweetOK(phrase) {
-      if (!wordfilter.blacklisted(phrase) && phrase !== undefined && phrase !== "" && tweetLengthOK(phrase)){
-        return true;
-      } else {
-        return false;
-      }
-  }
+//functions
+function tweetOK(phrase) {
+  return phrase !== undefined && phrase !== "" && tweetLengthOK(phrase);
+}
 
-  function tweetLengthOK(phrase) {
-      if (phrase.length <= 130){
-        return true;
-      } else {
-        return false;
-      }
-  }
+function tweetLengthOK(phrase) {
+  return phrase.length <= 140;
+}
 
-function pickTweet(){
-      var tweetText = myText.pick();
-      if (tweetOK(tweetText)) {
-        return tweetText;
-      }
-      else {
-        tweetText = pickTweet();
-      }
+function pickTweet(oldTweets, n) {
+  var lastTweet = oldTweets[0].text;
+  var idx = myText.indexOf(lastTweet) + 1 + n;
+  if (idx >= myText.length) return;
+  var tweetText = myText[idx];
+
+  if (tweetOK(tweetText)) {
+    return tweetText;
   }
+  else {
+    return pickTweet(n+1);
+  }
+}
+
+function post(textToTweet) {
+  T.post('statuses/update', { status: textToTweet }, function(err, reply) {
+    if (err) {
+      console.log('error:', err);
+      context.fail();
+    }
+    else {
+      console.log('tweet:', reply);
+      context.succeed();
+    }
+  });
+}
 
 exports.handler = function myBot(event, context) {
+  var getOptions = { screen_name: config.screen_name, count: 1 };
+  T.get('statuses/user_timeline', getOptions, function(err, oldTweets) {
+    if (err || !oldTweets.length) {
+      console.log('error:', err);
+      context.fail();
+      return;
+    }
 
-    var textToTweet = pickTweet();
-  
-  	T.post('statuses/update', { status: textToTweet }, function(err, reply) {
-              if (err) {
-                console.log('error:', err);
-                context.fail();
-              }
-              else {
-                console.log('tweet:', reply);
-                context.succeed();
-              }
-            });
-  };
+    var textToTweet = pickTweet(oldTweets, 0);
+    if (!textToTweet) {
+      console.log('no more okay tweets');
+      context.fail();
+      return;
+    }
+
+    post(textToTweet);
+  });
+};
 
